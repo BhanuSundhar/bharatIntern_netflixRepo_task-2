80e23e701145c0cefe69f65ff48f7011061ab5a1
Bhanusundhhar <kuradabhanusundhar@gmail.com>
2024-06-11 11:44:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+function funOne(){
+    let x = document.getElementById('collapseExample1');
+    x.innerHTML="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, neque nam fuga consequuntur in voluptas repellendus at voluptatem vel esse sit harum doloribus libero quam sunt impedit. Deleniti, sit iste.";
+    if(document.getElementById('collapseExample1').hidden==true){
+        x.hidden=false;
+    }
+    else{
+        x.hidden=true;
+    }
+}
+function funTwo(){
+    let x = document.getElementById('collapseExample2');
+    x.innerHTML="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, neque nam fuga consequuntur in voluptas repellendus at voluptatem vel esse sit harum doloribus libero quam sunt impedit. Deleniti, sit iste.";
+    if(document.getElementById('collapseExample2').hidden==true){
+        x.hidden=false;
+    }
+    else{
+        x.hidden=true;
+    }
+}
+function funThree(){
+    let x = document.getElementById('collapseExample3');
+    x.innerHTML="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, neque nam fuga consequuntur in voluptas repellendus at voluptatem vel esse sit harum doloribus libero quam sunt impedit. Deleniti, sit iste.";
+    if(document.getElementById('collapseExample3').hidden==true){
+        x.hidden=false;
+    }
+    else{
+        x.hidden=true;
+    }
+}
+function funFour(){
+    let x = document.getElementById('collapseExample4');
+    x.innerHTML="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, neque nam fuga consequuntur in voluptas repellendus at voluptatem vel esse sit harum doloribus libero quam sunt impedit. Deleniti, sit iste.";
+    if(document.getElementById('collapseExample4').hidden==true){
+        x.hidden=false;
+    }
+    else{
+        x.hidden=true;
+    }
+}
+function funFive(){
+    let x = document.getElementById('collapseExample5');
+    x.innerHTML="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, neque nam fuga consequuntur in voluptas repellendus at voluptatem vel esse sit harum doloribus libero quam sunt impedit. Deleniti, sit iste.";
+    if(document.getElementById('collapseExample5').hidden==true){
+        x.hidden=false;
+    }
+    else{
+        x.hidden=true;
+    }
+}
+function funSix(){
+    let x = document.getElementById('collapseExample6');
+    x.innerHTML="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, neque nam fuga consequuntur in voluptas repellendus at voluptatem vel esse sit harum doloribus libero quam sunt impedit. Deleniti, sit iste.";
+    if(document.getElementById('collapseExample6').hidden==true){
+        x.hidden=false;
+    }
+    else{
+        x.hidden=true;
+    }
+}
